724214fb4e33971012e3fd207939cf8bcb7cc833
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-30 13:52:33 +0100

    changes in scss

--- src/app.js
+++ src/app.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import "./styles/styles.scss";
 import "normalize.css/normalize.css";
 import Main from "./components/Main";
-import Navigation from "./components/Navigation";
 
 const Page = () => (
   <div className="page">
-    <Navigation />
     <Main />
   </div>
 );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,20 @@
 import React from "react";
 import background_image from "../images/image_summer.png";
 import Newsletter from "./Newsletter";
+import Navigation from "./Navigation";
 
 const Main = () => (
-  <div
-    className="background"
-    style={{
-      backgroundImage: `url(${background_image})`,
-      backgroundRepeat: `no-repeat`,
-    }}
-  >
-    <Newsletter />
+  <div>
+    <Navigation />
+    <div
+      className="background"
+      style={{
+        backgroundImage: `url(${background_image})`,
+        backgroundRepeat: `no-repeat`,
+      }}
+    >
+      <Newsletter />
+    </div>
   </div>
 );
 
